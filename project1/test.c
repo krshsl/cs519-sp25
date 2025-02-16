640caa60027244dd03025e6b3e9975b6a403ce07
escalate <escalate@node0.project1.rise-pg0.cloudlab.umass.edu>
2025-02-16 10:58:43 -0500

    removed debug msg, updated test suite

--- project1/test.c
+++ project1/test.c
@@ -10,7 +10,17 @@
 #define DATA_SIZE 1024*sizeof(DATA_TYPE)
 #define ITERS 1
 
+#include <stdio.h>
+#include <sys/time.h>
+
+long long current_timestamp_micros() {
+    struct timeval tv;
+    gettimeofday(&tv, NULL);
+    return (long long)tv.tv_sec * 1000000LL + tv.tv_usec;
+}
+
 int testcall() {
+    long long start_time, end_time;
     long double averageTime = 0;
     long success, failure, iter;
     success = failure = 0;
@@ -21,18 +31,16 @@ int testcall() {
         if(data == NULL || check == NULL) break;
         memset(data, 4, DATA_SIZE);
         memset(check, 1, DATA_SIZE);
-        clock_gettime(CLOCK_REALTIME, &start);
+        start_time = current_timestamp_micros();
         
         if(syscall(SYS_APP_HELPER, data, DATA_SIZE) == -1) {
-            printf("Error: %d::%s\n", errno, strerror(errno));
             free(data);
             free(check);
             failure++;
             continue;
         }
-        clock_gettime(CLOCK_REALTIME, &end);
-        long timeElapsed = (end.tv_sec - start.tv_sec) * 1000000000L + (end.tv_nsec - start.tv_nsec);
-        averageTime += timeElapsed;
+        end_time = current_timestamp_micros();
+        averageTime += end_time - start_time;
         
         if (memcmp(data, check, DATA_SIZE) == 0) {
             success++;
@@ -45,7 +53,7 @@ int testcall() {
 
     printf("Total Iters: %ld, Success Iters: %ld, Failure Iters: %ld\n", iter, success, failure);
     printf("Success Rate: %f, Failure Rate: %f\n", (double)success/iter, (double)failure/iter);
-    printf("Average Time taken: %Lf nanoseconds\n", averageTime/iter);
+    printf("Average Time taken: %Lf microseconds\n", averageTime/iter);
     return (int) 1;
 }
 
