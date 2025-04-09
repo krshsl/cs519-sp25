d86d96fb8fdc2b99ad4cf2f8bc27a5a67c387797
escalate <escalate@node0.escalate-250824.rise-pg0.utah.cloudlab.us>
2025-04-09 15:10:28 -0600

    Apply Patch added

---
+++ project2/apply_patch.sh
@@ -0,0 +1,20 @@
+#! /bin/bash
+p21=../project2/linux-5.15.0
+patch_dirs=(arch/x86/entry/syscalls/syscall_64.tbl include/linux/syscalls.h include/linux/memory.h mm/memory.c mm/Makefile)
+for i in ${!patch_dirs[@]}
+do
+    pF="${p21}/${patch_dirs[i]}"
+    echo "${pF}"
+    if [ -f "${pF}.orig" ]; then
+        cp "${pF}" "${pF}.orig"
+    fi
+done
+
+for pfile in p2_patch/*; do
+    if [ -f "$pfile" ]; then 
+        echo "$pfile"
+        git apply --stat $pfile
+        git apply --check $pfile
+        git apply $pfile
+    fi 
+done
