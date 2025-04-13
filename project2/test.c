9887387539b4dfb8a1124a1b6d1c9d0c7e1ba674
escalate <escalate@node0.escalate-250824.rise-pg0.utah.cloudlab.us>
2025-04-12 19:45:37 -0600

    threading enabled for test suite, additional handlers added to get and print detail

--- project2/test.c
+++ project2/test.c
@@ -1,3 +1,4 @@
+#include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -14,8 +15,18 @@
 #define sys_disable_extents 336
 #endif
 
-#define DEFAULT_BUF_SIZE    1048576
-#define MAX_ITERS           1000
+#ifndef sys_get_ex_count
+#define sys_get_ex_count 337
+#endif
+
+#ifndef sys_print_pid
+#define sys_print_pid 338
+#endif
+
+// #define DEFAULT_BUF_SIZE    1048576
+#define DEFAULT_BUF_SIZE    409600
+#define MAX_ITERS           1
+#define MAX_PROCS           4
 
 static long get_time_us(void) {
     struct timeval tv;
@@ -26,34 +37,121 @@ static long get_time_us(void) {
     return (tv.tv_sec * 1000000L) + tv.tv_usec;
 }
 
-void init_pages(size_t buf_size) {
+void *create_bufs(void *arg) {
+    if (syscall(sys_print_pid) < 0) {
+        perror("syscall sys_print_pid (test)");
+        exit(EXIT_FAILURE);
+    }
+
+    size_t *buf_size = (size_t*)arg;
+    char *buffer = (char *)malloc(*buf_size);
+    if (!buffer) {
+        perror("malloc");
+        return NULL;
+    }
+    memset(buffer, 4, *buf_size); // this should touch all pages...
+    memset(buffer, 1, *buf_size); // this should modify all pages...
+    return buffer;
+}
+
+void init_pages(size_t buf_size, int *extns, int *pages) {
+    void *buffer;
+    *extns = 0;
+    *pages = 0;
 #ifdef USE_SYSCALL
     if (syscall(sys_enable_extents) < 0) {
         perror("syscall enable_extents (test)");
         exit(EXIT_FAILURE);
     }
 #endif
-
-    char *buffer = (char *)malloc(buf_size);
-    if (!buffer) {
-        perror("malloc");
+    buffer = create_bufs(&buf_size);
+#ifdef USE_SYSCALL
+    if (syscall(sys_get_ex_count, extns, pages) < 0) {
+        perror("syscall disable_extents (test)");
         exit(EXIT_FAILURE);
     }
-    memset(buffer, 4, buf_size); // this should touch all pages...
 
-#ifdef USE_SYSCALL
     if (syscall(sys_disable_extents) < 0) {
         perror("syscall disable_extents (test)");
-        free(buffer);
         exit(EXIT_FAILURE);
     }
+#endif
+    if (buffer) {
+        free(buffer);
+    } else {
+        exit(EXIT_FAILURE);
+    }
+}
+
+void repeat_oprs(int iter, size_t buf_size) {
+    long start_time, end_time;
+    long double total_time = 0;
+    int extns_cnt, pages_cnt, extns, pages, i;
+    extns_cnt = pages_cnt = i = 0;
+    start_time = get_time_us();
+    for (; i < iter; i++) {
+        init_pages(buf_size, &extns, &pages);
+        extns_cnt += extns;
+        pages_cnt += pages;
+    }
+    end_time = get_time_us();
+    total_time = (end_time - start_time)/iter;
+    printf("Total time: %Lf microseconds for %d iteration\n", total_time, iter);
+#ifdef USE_SYSCALL
+    printf("For %d iterations: %d extents and %d pages were used in total\n", iter, extns_cnt, pages_cnt);
+#endif
+}
+
+void multi_oprs(size_t buf_size) {
+    long start_time, end_time;
+    long double total_time = 0;
+    int extns_cnt, pages_cnt, i;
+    pthread_t ptid[MAX_PROCS];
+    void **buffers = malloc(MAX_PROCS*sizeof(void*));
+    memset(buffers, 0, MAX_PROCS*sizeof(void*));
+    start_time = get_time_us();
+#ifdef USE_SYSCALL
+    if (syscall(sys_enable_extents) < 0) {
+        perror("syscall enable_extents (test)");
+        exit(EXIT_FAILURE);
+    }
+#endif
+    for (i = 0; i < MAX_PROCS; i++) {
+        pthread_create(&ptid[i], NULL, &create_bufs, &buf_size);
+    }
+    for (i = 0; i < MAX_PROCS; i++) {
+        pthread_join(ptid[i], &buffers[i]);
+        if (buffers[i] == NULL) {
+            printf("Some error occurred...\n");
+        }
+    }
+#ifdef USE_SYSCALL
+    if (syscall(sys_get_ex_count, extns_cnt, pages_cnt) < 0) {
+        perror("syscall disable_extents (test)");
+        exit(EXIT_FAILURE);
+    }
+
+    if (syscall(sys_disable_extents) < 0) {
+        perror("syscall disable_extents (test)");
+        exit(EXIT_FAILURE);
+    }
+#endif
+    for (i = 0; i < MAX_PROCS; i++) {
+        if (buffers[i]) {
+            free(buffers[i]);
+        }
+    }
+    free(buffers);
+    end_time = get_time_us();
+    total_time = end_time - start_time;
+    printf("Total time: %Lf microseconds for %d threads\n", total_time, MAX_PROCS);
+#ifdef USE_SYSCALL
+    printf("For %d threads: %d extents and %d pages were used in total\n", MAX_PROCS, extns_cnt, pages_cnt);
 #endif
 }
 
 int main(int argc, char **argv) {
     int i;
-    long start_time, end_time;
-    long double total_time = 0;
     size_t buf_size = DEFAULT_BUF_SIZE;
 
     if (argc > 1) {
@@ -65,18 +163,9 @@ int main(int argc, char **argv) {
     }
     printf("Using buffer size: %zu bytes\n", buf_size);
 
-    start_time = get_time_us();
-    init_pages(buf_size);
-    end_time = get_time_us();
-    total_time = end_time - start_time;
-    printf("Total time: %Lf microseconds for single iteration\n", total_time);
+    // repeat_oprs(1, buf_size);
+    // repeat_oprs(MAX_ITERS, buf_size);
+    multi_oprs(buf_size);
 
-    start_time = get_time_us();
-    for (int i = 0; i < MAX_ITERS; i++) {
-        init_pages(buf_size);
-    }
-    end_time = get_time_us();
-    total_time = (end_time - start_time)/MAX_ITERS;
-    printf("Total time: %Lf microseconds for %d iteration\n", total_time, MAX_ITERS);
     return EXIT_SUCCESS;
 }
