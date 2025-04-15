64fde94f155b4ba0cb136e84eaf806ff672cba53
escalate <escalate@node0.escalate-250824.rise-pg0.utah.cloudlab.us>
2025-04-15 00:58:16 -0600

    automating testing and outfile gen

---
+++ project2/test/run_tests.sh
@@ -0,0 +1,48 @@
+#! /bin/bash
+#logfile=test.out
+
+buf_sizes=(4096 65536 1048576 16777216 268435456)
+threads=(2 4 8 16 32 64)
+
+run_sim() {
+    iters=5000
+    src=../../../
+    stat="sudo perf stat -e faults -r $iters --table --output"
+    for buf in ${buf_sizes[@]}
+    do
+        mkdir $buf
+        cd $buf
+        mkdir 1
+        cd  1
+        $stat st.out $src/st.o 1 $buf > st.csv
+        $stat est.out $src/est.o 1 $buf > est.csv
+        cd ..
+        for proc in ${threads[@]}
+        do
+            mkdir $proc
+            cd $proc
+            $stat st.out $src/ast.o $proc $buf > st.csv
+            $stat est.out $src/east.o $proc $buf > est.csv
+            cd ..
+        done
+        cd ..
+    done
+}
+
+echo "running mmap fault"
+make clean
+make MIN_PRINT=1 -j 16 all
+rm -rf mmap
+mkdir mmap
+cd mmap
+run_sim
+cd ..
+
+echo "running fmap fault"
+make clean
+make MIN_PRINT=1 IS_FILE=1 -j 16 all
+rm -rf fmap
+mkdir fmap
+cd fmap
+run_sim
+cd ..
