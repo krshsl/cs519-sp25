970e08bc8600bc91588dbc2c66f61811e577e6b9
escalate <escalate@node0.p3.rise-pg0.utah.cloudlab.us>
2025-05-08 15:28:59 -0600

    inactive coop added on percpu and tracker per task

--- project3/set_patch_env.sh
+++ project3/set_patch_env.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 export p31=./linux-5.15.0
-export patch_dirs=(arch/x86/entry/syscalls/syscall_64.tbl include/linux/syscalls.h kernel/sched/core.c)
-export out_dirs=(syscall_64.patch syscall_h.patch sched_core_c.patch)
+export patch_dirs=(arch/x86/entry/syscalls/syscall_64.tbl include/linux/syscalls.h kernel/sched/core.c kernel/fork.c init/init_task.c include/linux/sched.h)
+export out_dirs=(syscall_64.patch syscall_h.patch sched_core_c.patch fork_c.patch init_task_c.patch sched_h.patch)
