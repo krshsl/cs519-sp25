c7fb91bb1e4a3ba37762326793a1bd94b7642b89
escalate <escalate@node0.p3.rise-pg0.utah.cloudlab.us>
2025-05-09 01:02:27 -0600

    preempt based on vruntime

--- project3/set_patch_env.sh
+++ project3/set_patch_env.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 export p31=./linux-5.15.0
-export patch_dirs=(arch/x86/entry/syscalls/syscall_64.tbl include/linux/syscalls.h kernel/sched/core.c kernel/fork.c init/init_task.c include/linux/sched.h kernel/exit.c)
-export out_dirs=(syscall_64.patch syscall_h.patch sched_core_c.patch fork_c.patch init_task_c.patch sched_h.patch exit_c.patch)
+export patch_dirs=(arch/x86/entry/syscalls/syscall_64.tbl include/linux/syscalls.h kernel/sched/core.c kernel/fork.c init/init_task.c include/linux/sched.h kernel/sched/fair.c kernel/exit.c)
+export out_dirs=(syscall_64.patch syscall_h.patch sched_core_c.patch fork_c.patch init_task_c.patch sched_h.patch sched_fair_c.patch exit_c.patch)
